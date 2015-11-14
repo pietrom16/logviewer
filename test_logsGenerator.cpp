7450b2d05712eed83bcd886bd2a8c78595d5562c
PietroM16 <pietrom16@gmail.com>
2015-11-14 00:49:36 +0000

    - Added a test program that generates test log files.

---
+++ test_logsGenerator.cpp
@@ -0,0 +1,71 @@
+/******************************************************************************
+ * test_logsGenerator.cpp
+ *
+ * Test application to generate random logs.
+ *
+ * Copyright (C) 2012-2015 Pietro Mele
+ * Released under a GPL 3 license.
+ *
+ * pietrom16@gmail.com
+ *
+ *****************************************************************************/
+
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+std::string LogTime();
+std::string LogDate();
+
+
+int main()
+{
+	std::string log;
+	std::string logFile = "test.log";
+	//+OK std::ofstream ofs(logFile);
+
+	// Log levels (a custom levels enum can be used)
+	static const int nLogLevels = 8;
+	const char       logLevelTags[nLogLevels][9] { "  ----  ", "VERBOSE ", "DETAIL  ", "INFO    ", "WARNING ", "ERROR   ", "CRITICAL", "FATAL   " };
+	int              level = 0;
+
+	struct timespec pause;
+	pause.tv_sec  = 0;
+	pause.tv_nsec = 0.25e9;
+
+
+	while(true)
+	{
+		log.clear();
+		log += LogTime();
+		log += LogDate();
+		log += std::string("Test log message");
+
+		//+ ofs
+		std::cout << LogTime() << LogDate() << logLevelTags[level] << log << std::endl;
+
+		std::cout << "." << std::flush;
+		nanosleep(&pause, NULL);
+	}
+
+	return 0;
+}
+
+
+std::string LogTime() {
+	float t = float(std::clock())/CLOCKS_PER_SEC;
+	const size_t sz = 16;
+	char ct[sz];
+	std::snprintf(ct, sz, "% 7.3f  ", t);
+	return std::string(ct);
+}
+
+
+std::string LogDate() {
+	std::time_t t = std::time(nullptr);
+	char mbstr[32];
+	std::strftime(mbstr, sizeof(mbstr), "%F %T  ", std::localtime(&t));
+	return std::string(mbstr);
+}
+
