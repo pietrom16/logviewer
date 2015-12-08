5c2e67b4af9f56447dd8dc58af621569e8e1ff86
PietroM16 <pietrom16@gmail.com>
2015-12-08 23:45:51 +0000

    - Minor change.

--- logLevels.cpp
+++ logLevels.cpp
@@ -18,105 +18,105 @@
 
 namespace LogViewer {
 
-	using namespace textModeFormatting;
+using namespace textModeFormatting;
 
 
-	int LogLevels::InitLogLevels()
+int LogLevels::InitLogLevels()
+{
+	/* Level tag (case insensitive), Level value */
+	levels.push_back(TagLevel("VERBOSE",   1));
+	levels.push_back(TagLevel("TRACE",     1));
+	levels.push_back(TagLevel("DETAIL",    2));
+	levels.push_back(TagLevel("DEBUG",     2));
+	levels.push_back(TagLevel("INFO",      3));
+	levels.push_back(TagLevel("NOTICE",    3));
+	levels.push_back(TagLevel("WARNING",   4));
+	levels.push_back(TagLevel("WARN",      4));
+	levels.push_back(TagLevel("ERROR",     5));
+	levels.push_back(TagLevel("ERR",       5));
+	levels.push_back(TagLevel("CRITICAL",  6));
+	levels.push_back(TagLevel("SEVERE",    6));
+	levels.push_back(TagLevel("ALERT",     6));
+	levels.push_back(TagLevel("FATAL",     7));
+	levels.push_back(TagLevel("EMERGENCY", 7));
+
+	return levels.size();
+}
+
+
+int LogLevels::InitLogLevels(const std::vector<TagLevel> &_levels){}
+
+
+int LogLevels::AddLogLevels(const std::vector<TagLevel> &_levels){}
+
+
+int LogLevels::AddLogLevel(const TagLevel &_level){}
+
+
+int LogLevels::ClearLogLevels()
+{
+	levels.clear();
+	return levels.size();
+}
+
+
+int LogLevels::GetVal(const std::string &_tag)
+{
+	if (isdigit(_tag[0]))
+		// A number, use it directly
+		return std::atoi(_tag.c_str()) % nLevels;
+
+	const std::string tag = LogLevels::ToUppercase(_tag);
+
+	if (tag[0] == 'L' || tag == "NO_LEVEL")
+		// The 'L' special character
+		return nLevels - 1;
+
+	// Check for string level
+
+	size_t i = 0;
+
+	while (i < levels.size())
 	{
-		/* Level tag (case insensitive), Level value */
-		levels.push_back(TagLevel("VERBOSE",   1));
-		levels.push_back(TagLevel("TRACE",     1));
-		levels.push_back(TagLevel("DETAIL",    2));
-		levels.push_back(TagLevel("DEBUG",     2));
-		levels.push_back(TagLevel("INFO",      3));
-		levels.push_back(TagLevel("NOTICE",    3));
-		levels.push_back(TagLevel("WARNING",   4));
-		levels.push_back(TagLevel("WARN",      4));
-		levels.push_back(TagLevel("ERROR",     5));
-		levels.push_back(TagLevel("ERR",       5));
-		levels.push_back(TagLevel("CRITICAL",  6));
-		levels.push_back(TagLevel("SEVERE",    6));
-		levels.push_back(TagLevel("ALERT",     6));
-		levels.push_back(TagLevel("FATAL",     7));
-		levels.push_back(TagLevel("EMERGENCY", 7));
-
-		return levels.size();
+		if (tag == levels[i].tag)
+			return levels[i].level;
+		++i;
 	}
 
+	// Nothing found; use a random mapping
 
-	int LogLevels::InitLogLevels(const std::vector<TagLevel> &_levels){}
-
-
-	int LogLevels::AddLogLevels(const std::vector<TagLevel> &_levels){}
-
-
-	int LogLevels::AddLogLevel(const TagLevel &_level){}
+	return LogLevelMapping(tag);
+}
 
 
-	int LogLevels::ClearLogLevels()
+std::string LogLevels::GetTag(int _val)
+{
+	size_t i = 0;
+
+	while (i < levels.size())
 	{
-		levels.clear();
-		return levels.size();
+		if (_val == levels[i].level)
+			return levels[i].tag;
+		++i;
 	}
 
+	return levels[i - 1].tag;
+}
 
-	int LogLevels::GetVal(const std::string &_tag)
+
+int LogLevels::LogLevelMapping(const std::string &_tag)
+{
+	int colorCode = 0;
+
+	for (size_t i = 0; i < _tag.size(); ++i)
 	{
-		if (isdigit(_tag[0]))
-			// A number, use it directly
-			return std::atoi(_tag.c_str()) % nLevels;
-
-		const std::string tag = LogLevels::ToUppercase(_tag);
-
-		if (tag[0] == 'L' || tag == "NO_LEVEL")
-			// The 'L' special character
-			return nLevels - 1;
-
-		// Check for string level
-
-		size_t i = 0;
-
-		while (i < levels.size())
-		{
-			if (tag == levels[i].tag)
-				return levels[i].level;
-			++i;
-		}
-
-		// Nothing found; use a random mapping
-
-		return LogLevelMapping(tag);
+		colorCode += _tag[i];		//+TODO: more randomness
 	}
 
+	colorCode = colorCode % 7;		// use the first 7 colors only
 
-	std::string LogLevels::GetTag(int _val)
-	{
-		size_t i = 0;
-
-		while (i < levels.size())
-		{
-			if (_val == levels[i].level)
-				return levels[i].tag;
-			++i;
-		}
-
-		return levels[i - 1].tag;
-	}
-
-
-	int LogLevels::LogLevelMapping(const std::string &_tag)
-	{
-		int colorCode = 0;
-
-		for (size_t i = 0; i < _tag.size(); ++i)
-		{
-			colorCode += _tag[i];		//+TODO: more randomness
-		}
-
-		colorCode = colorCode % 7;		// use the first 7 colors only
-
-		return colorCode;
-	}
+	return colorCode;
+}
 
 
 // Return the log level tag in a log message; empty string if not found
