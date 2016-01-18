1ceef5a8050f9dde7d4bd886d96a12cf2d9e8a95
PietroM16 <pietrom16@gmail.com>
2016-01-18 18:47:30 +0000

    - Added new LogFormatter class.

---
+++ LogFormatter.cpp
@@ -0,0 +1,25 @@
+/******************************************************************************
+ * LogFormatter.cpp
+ *
+ * Get a plain log message and format it according to its destination (e.g. text, HTML, ...).
+ *
+ * Copyright (C) 2012-2016 Pietro Mele
+ * Released under a GPL 3 license.
+ *
+ * pietrom16@gmail.com
+ *
+ *****************************************************************************/
+
+#include "LogFormatter.hpp"
+
+
+namespace LogViewer {
+
+
+LogFormatter::LogFormatter()
+{
+
+}
+
+
+} // LogViewer
---
+++ LogFormatter.hpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+ * LogFormatter.hpp
+ *
+ * Get a plain log message and format it according to its destination (e.g. text, HTML, ...).
+ *
+ * Copyright (C) 2012-2016 Pietro Mele
+ * Released under a GPL 3 license.
+ *
+ * pietrom16@gmail.com
+ *
+ *****************************************************************************/
+
+#ifndef LOG_FORMATTER_HPP
+#define LOG_FORMATTER_HPP
+
+
+namespace LogViewer {
+
+
+class LogFormatter
+{
+public:
+	LogFormatter();
+};
+
+
+} // LogViewer
+
+
+#endif // LOG_FORMATTER_HPP
