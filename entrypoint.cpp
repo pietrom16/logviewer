f01af8db01845e4c23c516f60715a3a44a602576
PietroM16 <pietrom16@gmail.com>
2016-03-11 17:03:06 +0000

    - Updated version number.

--- entrypoint.cpp
+++ entrypoint.cpp
@@ -21,7 +21,7 @@
 
 #include "logviewer.hpp"
 
-const int version = 4, subversion = 6, subsubversion = 0;
+const int version = 4, subversion = 99, subsubversion = 0;
 /* Versioning conventions:
  *	- Even subversion number: stable version.
  *	- Odd subversion number: unstable/development version.
