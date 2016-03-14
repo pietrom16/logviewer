d6afad52eaf59da253614ff989bfcdd076fc75d8
PietroM16 <pietrom16@gmail.com>
2016-03-14 00:16:16 +0000

    - Version 5.0.2

--- logviewer.hpp
+++ logviewer.hpp
@@ -55,7 +55,7 @@ public:
 	void  PrintHelp(const Utilities::ProgArgs &_args, const char* _progName, LogLevels *_logLevels = 0);
 	void  PrintVersion(const char* _progName);
 
-	static const int  version = 5, subversion = 0, subsubversion = 1;
+	static const int  version = 5, subversion = 0, subsubversion = 2;
 	/* Versioning conventions:
 	 *	- Even subversion number: stable version.
 	 *	- Odd subversion number: unstable/development version.
