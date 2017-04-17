fa0e4055be68d38853ff2a2bf84e8939ff41732b
PietroM16 <pietrom16@gmail.com>
2017-04-17 16:13:58 +0100

    - Added a header and an implementation file for Qt GUI.

---
+++ QLogViewer.cpp
@@ -0,0 +1,14 @@
+/******************************************************************************
+ * QLogViewer.cpp
+ *
+ * Display log files in real time through the Qt GUI.
+ *
+ * Copyright (C) 2012-2017 Pietro Mele
+ * Released under a GPL 3 license.
+ *
+ * pietrom16@gmail.com
+ *
+ *****************************************************************************/
+
+#include "QLogViewer.hpp"
+
---
+++ QLogViewer.hpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+ * QLogViewer.hpp
+ *
+ * Display log files in real time through the Qt GUI.
+ *
+ * Copyright (C) 2012-2017 Pietro Mele
+ * Released under a GPL 3 license.
+ *
+ * pietrom16@gmail.com
+ *
+ *****************************************************************************/
+
+#ifndef QLOGVIEWER_HPP
+#define QLOGVIEWER_HPP
+
+
+
+#endif // QLOGVIEWER_HPP
